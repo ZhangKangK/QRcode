a15c64ed0ed1e75d8a4baba63696aaa923eccbc3
kangkang <18325516988@163.com>
2018-04-22 17:24:27 +0800

    【界面优化】调整相机窗口和配置界面窗口到合适的大小【张康康】

--- splitterWidget/splitterWidget/splitterwidget.cpp
+++ splitterWidget/splitterWidget/splitterwidget.cpp
@@ -5,6 +5,13 @@
 SplitterWidget::SplitterWidget(QWidget *parent)
     : QMainWindow(parent)
 {
+    layoutWidget();
+    createCameraWidget();
+    createConfigWidget();
+}
+
+void SplitterWidget::layoutWidget()
+{
     QSplitter *mainSplitter = new QSplitter(Qt::Horizontal);
     QTextEdit *leftEdit = new QTextEdit(QObject::tr("左窗口"), mainSplitter);
     leftEdit->setAlignment(Qt::AlignCenter);
@@ -12,10 +19,12 @@ SplitterWidget::SplitterWidget(QWidget *parent)
     QSplitter *rightSplitter = new QSplitter(Qt::Vertical, mainSplitter);
     rightSplitter->setOpaqueResize(false);
 
-    QTextEdit *upEdit = new QTextEdit(QObject::tr("上窗口", rightSplitter));
-    upEdit->setAlignment(Qt::AlignCenter);
 
-    QTextEdit *bottomEdit = new QTextEdit(QObject::tr("下窗口", rightSplitter));
+
+    //QTextEdit *upEdit = new QTextEdit(QObject::tr("上窗口"), rightSplitter);
+    //cameraWidget->setAlignment(Qt::AlignCenter);
+
+    QTextEdit *bottomEdit = new QTextEdit(QObject::tr("下窗口"), rightSplitter);
     bottomEdit->setAlignment(Qt::AlignCenter);
 
     mainSplitter->setStretchFactor(1, 1);
@@ -23,6 +32,41 @@ SplitterWidget::SplitterWidget(QWidget *parent)
 
     setCentralWidget(mainSplitter);
     mainSplitter->show();
+}
+
+void SplitterWidget::createCameraWidget()
+{
+    camera = new QCamera();
+    cameraViewFinder = new QCameraViewfinder();
+    cameraImageCapture = new QCameraImageCapture(camera);
+
+    cameraWidget = new QDockWidget(tr("camera"), this);
+    //cameraWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
+
+    addDockWidget(Qt::RightDockWidgetArea, cameraWidget);
+    cameraWidget->setWidget(cameraViewFinder);
+    cameraWidget->setMaximumSize(WIDGET_MAX_SIZE);
+    cameraWidget->setMinimumSize(WIDGET_MIN_SIZE);
+
+    cameraImageCapture->setCaptureDestination(QCameraImageCapture::CaptureToFile);
+    camera->setCaptureMode(QCamera::CaptureStillImage);
+    camera->setViewfinder(cameraViewFinder);
+    camera->start();//启动摄像头
+}
+void SplitterWidget::createConfigWidget()
+{
+    configWidget = new QDockWidget(tr("config"), this);
+    configGroupBox = new QGroupBox(configWidget);
+    //configWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
+
+    configLabel = new QLabel(tr("save"));
+    addDockWidget(Qt::RightDockWidgetArea, configWidget);
+    configWidget->setWidget(configGroupBox);
+    configWidget->setMaximumSize(WIDGET_MAX_SIZE);
+    configWidget->setMinimumSize(WIDGET_MIN_SIZE);
+
+
+
 }
 
 SplitterWidget::~SplitterWidget()
--- splitterWidget/splitterWidget/splitterwidget.h
+++ splitterWidget/splitterWidget/splitterwidget.h
@@ -2,6 +2,15 @@
 #define SPLITTERWIDGET_H
 
 #include <QMainWindow>
+#include <QDockWidget>
+#include <QGroupBox>
+#include <QLabel>
+#include <QCamera>
+#include <QCameraImageCapture>
+#include <QCameraViewfinder>
+
+#define WIDGET_MAX_SIZE QSize(500, 500) //窗口最大尺寸
+#define WIDGET_MIN_SIZE QSize(100, 100) //窗口最小尺寸
 
 class SplitterWidget : public QMainWindow
 {
@@ -10,6 +19,21 @@ class SplitterWidget : public QMainWindow
 public:
     SplitterWidget(QWidget *parent = 0);
     ~SplitterWidget();
+
+    void layoutWidget();
+    void createCameraWidget();
+    void createConfigWidget();
+
+private:
+    QDockWidget *cameraWidget;  //相机窗口
+    QDockWidget *configWidget;  //配置界面
+
+    QLabel *configLabel;
+    QGroupBox *configGroupBox;
+
+    QCamera *camera;//系统摄像头设备
+    QCameraViewfinder *cameraViewFinder;//摄像头取景器部件
+    QCameraImageCapture *cameraImageCapture;//截图部件
 };
 
 #endif // SPLITTERWIDGET_H
